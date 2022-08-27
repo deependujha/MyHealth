42101f4f6ed90a5893b118337da5b373863da817
deependujha <deependujha21@gmail.com>
2022-08-27 16:52:37 +0530

    added and styled main page's intro text

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,7 +7,10 @@ import Main from "../components/mainPage/Main";
 export default function Home() {
   return (
     <div className="">
-      <div className={`${styles.main}`}>
+      <div className={`customText text-center mt-3`}>
+        MyHealth - Your health database
+      </div>
+      <div className={``}>
         <Main />
       </div>
     </div>
