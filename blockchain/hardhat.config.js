491743b2625c129ee55cd0dc87fa99d211260424
deependujha <deependujha21@gmail.com>
2022-08-27 14:25:28 +0530

    deployed on polygon mumbai chain

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,19 @@
+require("dotenv").config()
 require("@nomicfoundation/hardhat-toolbox");
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    matic: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.MATIC_PRIVATE_KEY}`],
+    },
+  },
 };
--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -7,19 +7,29 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const hospitalOrg = "0x2b8535524cb3381071590378973d2fa627ea933d";
+  const defenceOrg = "0xaf0f5f9d645cb00622f87fb6fc6f9970364aea48";
+  const insuranceOrg = "0x3cd2b592b9f30721050c4e0ac3a9c0fc89252128";
+  const dataAnalyticsOrg = "0xb7dcef078e52950d64efdfc8a19945f84f90a256";
 
-  await lock.deployed();
+  const MyHealth = await hre.ethers.getContractFactory("MyHealth");
+  const myHealth = await MyHealth.deploy(
+    hospitalOrg,
+    defenceOrg,
+    insuranceOrg,
+    dataAnalyticsOrg
+  );
+
+  await myHealth.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `MyHealth contract deployed on the polygon blockchain. Address: ${myHealth.address}`
   );
 }
 
